692ce73dd9c9babf5ae7f8c0869f14ea00deca3e
Daniel Vega <danve4@gmail.com>
2019-02-17 03:12:40 -0600

    Added timeuntil pipe

---
+++ cta-tracker/src/app/timeuntil.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { TimeuntilPipe } from './timeuntil.pipe';
+
+describe('TimeuntilPipe', () => {
+  it('create an instance', () => {
+    const pipe = new TimeuntilPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
