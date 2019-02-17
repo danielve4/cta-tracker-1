da1f5d3c28fe35eb40d895614000300f1485e051
Daniel Vega <danve4@gmail.com>
2019-02-17 03:50:32 -0600

    Saves current state and restores upon first load

--- cta-tracker/src/app/app.component.ts
+++ cta-tracker/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router, Event, NavigationEnd } from '@angular/router';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cta-tracker';
+  LS_SAVED_ROUTE: string = 'LS_SAVED_ROUTE';
+  firstLoad: boolean = true;
+  constructor(private router: Router) {
+    const savedCurrentRoute: string = localStorage.getItem(this.LS_SAVED_ROUTE);
+
+    if (this.firstLoad && savedCurrentRoute)
+      this.router.navigateByUrl(savedCurrentRoute);
+
+    this.firstLoad = false;
+
+    router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd)
+        localStorage.setItem(this.LS_SAVED_ROUTE, event.url);
+
+    });
+  }
 }
